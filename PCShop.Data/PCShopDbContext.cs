0e34f35f67b30b933f76e651387c86cba6aef600
Alexandr0w <alexandrow2505@gmail.com>
2025-06-24 02:54:55 +0300

    Uploaded project architecture

---
+++ PCShop.Data/PCShopDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace PCShop.Data
+{
+    public class PCShopDbContext : IdentityDbContext
+    {
+        public PCShopDbContext(DbContextOptions<PCShopDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ PCShop.Web/Program.cs
@@ -0,0 +1,62 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using PCShop.Data;
+
+namespace PCShop.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
+            
+            string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
+                ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            
+            builder.Services
+                .AddDbContext<PCShopDbContext>(options =>
+                {
+                    options.UseSqlServer(connectionString);
+                });
+
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+            builder.Services
+                .AddDefaultIdentity<IdentityUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                })
+                .AddEntityFrameworkStores<PCShopDbContext>();
+
+            builder.Services.AddControllersWithViews();
+
+            WebApplication? app = builder.Build();
+            
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
