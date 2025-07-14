6972c41083f1d27f461e30ce27235fe45b46a265
Alexandr0w <alexandrow2505@gmail.com>
2025-07-14 14:56:15 +0300

    Added new error messages

--- PCShop.GCommon/ErrorMessages.cs
+++ PCShop.GCommon/ErrorMessages.cs
@@ -7,5 +7,7 @@
         public const string EditProductErrorMessage = "Fatal error occurred while updating the product!";
         public const string NotModifyMessage = "Unable to modify the product.";
         public const string DeleteErrorMessage = "An error occurred while deleting the product.";
+        public const string RequiredIdMessage = "Product or Computer ID is required.";
+        public const string QuantityMustBeMessage = "Quantity must be greater than 0.";
     }
 }
