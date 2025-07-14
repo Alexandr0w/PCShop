3ce6ba730b5a83bfaefbb31dc0e0596c4b4c52ce
Alexandr0w <alexandrow2505@gmail.com>
2025-07-14 19:00:53 +0300

    Updated error messages

--- PCShop.GCommon/ErrorMessages.cs
+++ PCShop.GCommon/ErrorMessages.cs
@@ -4,12 +4,15 @@
     {
         public const string AddProductErrorMessage = "Fatal error occurred while adding a product.";
         public const string AddComputerErrorMessage = "Fatal error occurred while adding a computer.";
+
         public const string EditProductErrorMessage = "Fatal error occurred while updating the product.";
         public const string EditComputerErrorMessage = "Fatal error occurred while updating the computer.";
+        
         public const string DeleteProductErrorMessage = "An error occurred while deleting the product.";
         public const string DeleteComputerErrorMessage = "An error occurred while deleting the computer.";
+        
         public const string NotModifyMessage = "Unable to modify.";
-        public const string RequiredIdMessage = "Product or Computer ID is required.";
+        public const string RequiredIdMessage = "Product ID or Computer ID is required.";
         public const string QuantityMustBeMessage = "Quantity must be greater than 0.";
     }
 }
