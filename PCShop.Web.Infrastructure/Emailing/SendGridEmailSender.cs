4e078cc4b1298becc5531c5f18a87e7baa5049de
Alexandr0w <alexandrow2505@gmail.com>
2025-07-21 22:57:15 +0300

    Added email sender with SendGrid

---
+++ PCShop.Web.Infrastructure/Emailing/SendGridEmailSender.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace PCShop.Web.Infrastructure.Emailing
+{
+    public class SendGridEmailSender : IEmailSender
+    {
+        private readonly IConfiguration _configuration;
+
+        public SendGridEmailSender(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            string? apiKey = this._configuration["SendGrid:ApiKey"];
+            string? fromEmail = this._configuration["SendGrid:FromEmail"];
+            string? fromName = this._configuration["SendGrid:FromName"] ?? "PCShop";
+
+            SendGridClient client = new SendGridClient(apiKey);
+            EmailAddress from = new EmailAddress(fromEmail, fromName);
+            EmailAddress to = new EmailAddress(email);
+            SendGridMessage msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent: null, htmlContent: htmlMessage);
+            
+            await client.SendEmailAsync(msg);
+        }
+    }
+}
