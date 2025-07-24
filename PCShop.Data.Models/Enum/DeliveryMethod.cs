84cd41d61da6d8d429ece844e388feacbd1863af
Alexandr0w <alexandrow2505@gmail.com>
2025-07-24 20:30:50 +0300

    Added fix while confirm without delivery method

--- PCShop.Data.Models/Enum/DeliveryMethod.cs
+++ PCShop.Data.Models/Enum/DeliveryMethod.cs
@@ -1,7 +1,10 @@
-﻿namespace PCShop.Data.Models.Enum
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PCShop.Data.Models.Enum
 {
     public enum DeliveryMethod
     {
+        [Display(Name = "Select delivery method")]
         None = 0,
         Econt = 1,
         Speedy = 2,
