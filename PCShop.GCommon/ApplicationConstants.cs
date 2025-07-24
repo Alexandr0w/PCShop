807b000f9c5de7f2eb6e27adc9107d6f937f0fc8
Alexandr0w <alexandrow2505@gmail.com>
2025-07-24 20:34:47 +0300

    Changed some names of application constants

--- PCShop.GCommon/ApplicationConstants.cs
+++ PCShop.GCommon/ApplicationConstants.cs
@@ -3,11 +3,11 @@
     public static class ApplicationConstants
     {
         public const string AppDateFormat = "yyyy-MM-dd";
-        public const string OrderDateFormat = "dd.MM.yyyy HH:mm";
+        public const string DateAndTimeFormat = "dd.MM.yyyy HH:mm";
 
         public const string NoImageUrl = "no-image.jpg";
         public const string PriceSqlType = "decimal(18,2)";
-        public const string OrderDateDefaultSqlValue = "GETUTCDATE()";
+        public const string DefaultSqlValue = "GETUTCDATE()";
         public const string IsDeletedPropertyName = "IsDeleted";
 
         public const string AdminRoleName = "Admin";
